4a785279ffcd42ab31e8dcaca6028e6f38901d60
Mynul-Dev <mynul.ir@tudotechnologies.com>
2022-11-28 00:03:51 +0600

    pic rounded

--- src/Component/Header/Headers.js
+++ src/Component/Header/Headers.js
@@ -52,7 +52,7 @@ const Header = () => {
                 <div class="dropdown dropdown-end">
                     <label tabindex="0" class="btn btn-ghost btn-circle avatar">
                         <div class="w-10 rounded-full">
-                        <img className='h-14 rounded-xl' src='https://templates.iqonic.design/posdash/html/assets/images/user/1.png' alt="flag" />
+                        <img className='' src='https://templates.iqonic.design/posdash/html/assets/images/user/1.png' alt="flag" />
                         </div>
                     </label>
                     <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
--- src/Component/Notifications/Notifications.js
+++ src/Component/Notifications/Notifications.js
@@ -10,25 +10,25 @@ const Notifications = ({ display ,title}) => {
 
                 </div>
                 <div class="flex  items-center justify-between py-3 border-b border-gray-light pb-4">
-                    <div class="">
-                        <img class=" h-14 rounded-xl" src="https://templates.iqonic.design/posdash/html/assets/images/user/01.jpg" alt="01" />
+                    <div class="w-10 rounded-full">
+                        <img className='rounded-full'  src="https://templates.iqonic.design/posdash/html/assets/images/user/01.jpg" alt="01" />
                     </div>
                     <div >
-                        <div class="flex items-center justify-between">
+                        <div class="flex items-center gap-1 justify-between">
                             <h6 class="mb-0">Emma Watson</h6>
-                            <small className='ml-4'><b>12 : 47 pm</b></small>
+                            <small className=''><b>12 : 47 pm</b></small>
                         </div>
                         <small class="mb-0">Lorem ipsum dolor sit amet</small>
                     </div>
                 </div>
                 <div class="flex  items-center justify-between py-3  pb-4">
-                    <div class="">
-                        <img class=" h-14 rounded-xl" src="https://templates.iqonic.design/posdash/html/assets/images/user/02.jpg" alt="01" />
+                    <div class="w-10 rounded-full">
+                        <img className='rounded-full' src="https://templates.iqonic.design/posdash/html/assets/images/user/02.jpg" alt="01" />
                     </div>
                     <div >
                         <div class="flex items-center justify-between">
                             <h6 class="mb-0">Emma Watson</h6>
-                            <small className='ml-4'><b>12 : 47 pm</b></small>
+                            <small className=''><b>12 : 47 pm</b></small>
                         </div>
                         <small class="mb-0">Lorem ipsum dolor sit amet</small>
                     </div>
