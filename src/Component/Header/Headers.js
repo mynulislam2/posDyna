a6c820c2ffff11139cc5ccd4c31c6ef13e043393
Mynul-Dev <mynul.ir@tudotechnologies.com>
2022-11-27 17:55:33 +0600

    header pop up done

--- src/Component/Header/Headers.js
+++ src/Component/Header/Headers.js
@@ -6,45 +6,68 @@ import './Header.css'
 import Notifications from '../Notifications/Notifications';
 
 const Header = () => {
-const [isShowNotifi,setisShowNotifi] =useState(false)
-const [islShowMessage,setislShowMessage] =useState(false)
-    const HandleAddNewOrder=()=> {
-    Swal.fire({
-        title: 'New Order',
-        input: 'email',
-        inputLabel: 'Email',
-        confirmButtonText:"Create",
-        showCancelButton: true,
-        inputPlaceholder: 'Enter your email address'
-    })
-}
+    const [isShowNotifi, setisShowNotifi] = useState(false)
+    const [islShowMessage, setislShowMessage] = useState(false)
+    const HandleAddNewOrder = () => {
+        Swal.fire({
+            title: 'New Order',
+            input: 'email',
+            inputLabel: 'Email',
+            confirmButtonText: "Create",
+            showCancelButton: true,
+            inputPlaceholder: 'Enter your email address'
+        })
+    }
 
-return (
-    <div className='flex items-center justify-between h-20 w-ful shadow-md px-10'>
-        <input type="text" placeholder="Search here..." class="input input-bordered w-full max-w-xs" />
-        <div className='flex items-center gap-6'>
-            <button className="flex items-center gap-3 h-10 text-lg border border-gray-light  py-2 px-2 rounded-xl text-gray-300">
-                <img src={flag} alt="flag" />
-                <p>En</p>
-            </button>
-            <button onClick={() => HandleAddNewOrder()} className="flex items-center h-10 gap-1 text-lg  border-1 border border-gray-light py-2 px-2 rounded-xl text-gray-300">
-                <HiOutlinePlus />
-                <p>New Order</p>
-            </button>
-            <svg className="relative" onClick={()=>setislShowMessage(!islShowMessage)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-            </svg>
-            <Notifications display={islShowMessage}/>
-            <svg className="relative" onClick={()=>setisShowNotifi(!isShowNotifi)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-            </svg>
-            <Notifications display={isShowNotifi}/>
-            <img className='h-14 rounded-xl' src='https://templates.iqonic.design/posdash/html/assets/images/user/1.png' alt="flag" />
+    return (
+        <div className='flex items-center justify-between h-20 w-ful shadow-md px-10'>
+            <input type="text" placeholder="Search here..." class="input input-bordered w-full max-w-xs" />
+            <div className='flex items-center gap-8'>
+                <button className="flex items-center gap-3 h-10 text-lg border border-gray-light  py-2 px-2 rounded-xl text-gray-300">
+                    <img src={flag} alt="flag" />
+                    <p>En</p>
+                </button>
+                <button onClick={() => HandleAddNewOrder()} className="flex items-center h-10 gap-1 text-lg  border-1 border border-gray-light py-2 px-2 rounded-xl text-gray-300">
+                    <HiOutlinePlus />
+                    <p>New Order</p>
+                </button>
+                <svg  className="relative" onClick={()=>setislShowMessage(!islShowMessage)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                </svg>
+                <Notifications display={islShowMessage} />
+                <div className='flex content-between gap-1 relative'>
+                    <svg className="" onClick={() => setisShowNotifi(!isShowNotifi)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                    </svg>
+                    <div class="badge badge-primary absolute -top-3 left-6">1</div>
+
+                </div>
+
+
+                <Notifications display={isShowNotifi} />
+                <img className='h-14 rounded-xl' src='https://templates.iqonic.design/posdash/html/assets/images/user/1.png' alt="flag" />
+                {/* <div class="dropdown dropdown-end">
+                    <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                        <div class="w-10 rounded-full">
+                            <img src="https://placeimg.com/80/80/people" />
+                        </div>
+                    </label>
+                    <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a class="justify-between">
+                                Profile
+                                <span class="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div> */}
+            </div>
+
+
         </div>
-
-
-    </div>
-);
+    );
 };
 
 export default Header;
--- src/Component/Notifications/Notifications.js
+++ src/Component/Notifications/Notifications.js
@@ -1,16 +1,44 @@
 import React from 'react';
 
-const Notifications = ({display}) => {
+const Notifications = ({ display }) => {
     return (
-            <div className={`${display?'block':"hidden"} card w-80 bg-primary top-20 absolute text-primary-content`}>
-                <div class="card-body">
-                    <h2 class="card-title">Card title!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn">Buy Now</button>
+        <div tabIndex='1' className={`${display ? 'block' : "hidden"} card w-80 bg-base-100 border border-gray-light top-20 absolute text-primary-content`}>
+            <div class="card-body text-gray-dark">
+                <div className='flex justify-between items-center w-full border-b border-gray-light pb-4'>
+                    <h2 class="card-title">All Messages!</h2>
+                    <div class="badge badge-primary badge-outline">1</div>
+
+                </div>
+                <div class="flex  items-center justify-between py-3 border-b border-gray-light pb-4">
+                    <div class="">
+                        <img class=" h-14 rounded-xl" src="https://templates.iqonic.design/posdash/html/assets/images/user/01.jpg" alt="01" />
+                    </div>
+                    <div >
+                        <div class="flex items-center justify-between">
+                            <h6 class="mb-0">Emma Watson</h6>
+                            <small className='ml-4'><b>12 : 47 pm</b></small>
+                        </div>
+                        <small class="mb-0">Lorem ipsum dolor sit amet</small>
                     </div>
                 </div>
+                <div class="flex  items-center justify-between py-3  pb-4">
+                    <div class="">
+                        <img class=" h-14 rounded-xl" src="https://templates.iqonic.design/posdash/html/assets/images/user/02.jpg" alt="01" />
+                    </div>
+                    <div >
+                        <div class="flex items-center justify-between">
+                            <h6 class="mb-0">Emma Watson</h6>
+                            <small className='ml-4'><b>12 : 47 pm</b></small>
+                        </div>
+                        <small class="mb-0">Lorem ipsum dolor sit amet</small>
+                    </div>
+                </div>
+
+                <div class="card-actions justify-end">
+                    <button class="btn w-full">Buy Now</button>
+                </div>
             </div>
+        </div>
     );
 };
 
