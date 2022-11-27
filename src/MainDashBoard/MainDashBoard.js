62e090419fb9d24ca7057c0bfe95073484190634
Mynul-Dev <mynul.ir@tudotechnologies.com>
2022-11-28 01:30:01 +0600

    fix sidebar

--- src/MainDashBoard/MainDashBoard.js
+++ src/MainDashBoard/MainDashBoard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Sidebar, Menu, MenuItem,SubMenu } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
 import Header from '../Component/Header/Headers';
@@ -15,13 +15,16 @@ import { FaDashcube, FaCartPlus, FaListAlt, FaChartPie } from "react-icons/fa";
 
 const MainDashBoard = () => {
     return (
-        <ProSidebarProvider>
-            <div style={{ width: "100%", display: 'flex', height: '100%', minHeight: '100vh' }}>
-                <Sidebar style={{ width: "300px", boxShadow: "0px 16px 15px 0px rgb(0 0 0 / 7%)" }}>
-                    <div className='d-flex align-items-center justify-content-center'>
+        <ProSidebarProvider >
+
+            <div  style={{ display: 'flex', height: '100%', minHeight: '100vh' }}>
+
+                <Sidebar    style={{ boxShadow: "0px 16px 15px 0px rgb(0 0 0 / 7%)" }}>
+                    <div className='d-flex align-items-center justify-content-center '>
                         <img width="200px" src={brandlogo} className="py-4 img-fluid mx-auto" alt="" />
                     </div>
-                    <Menu className='py-2'>
+                    {/* <Menu className='fixed' style={{width:"249px"}} > */}
+                    <Menu  >
 
                         <SubMenu icon={<FaDashcube />} label="Dashboards">
                             <MenuItem> Pie charts</MenuItem>
@@ -32,7 +35,7 @@ const MainDashBoard = () => {
                             <MenuItem> Google maps</MenuItem>
                             <MenuItem> Open street maps</MenuItem>
                         </SubMenu>
-                        <SubMenu icon={<FaDashcube/>} label="Categories">
+                        <SubMenu icon={<FaDashcube />} label="Categories">
                             <MenuItem> Dark</MenuItem>
                             <MenuItem> Light</MenuItem>
                         </SubMenu>
@@ -45,7 +48,7 @@ const MainDashBoard = () => {
                             <MenuItem> Google maps</MenuItem>
                             <MenuItem> Open street maps</MenuItem>
                         </SubMenu>
-                        <SubMenu icon={<FaDashcube/>} label="Returns">
+                        <SubMenu icon={<FaDashcube />} label="Returns">
                             <MenuItem> Dark</MenuItem>
                             <MenuItem> Light</MenuItem>
                         </SubMenu>
@@ -58,7 +61,7 @@ const MainDashBoard = () => {
                             <MenuItem> Google maps</MenuItem>
                             <MenuItem> Open street maps</MenuItem>
                         </SubMenu>
-                        <SubMenu icon={<FaDashcube/>} label="Other Page">
+                        <SubMenu icon={<FaDashcube />} label="Other Page">
                             <MenuItem> Dark</MenuItem>
                             <MenuItem> Light</MenuItem>
                         </SubMenu>
@@ -66,6 +69,10 @@ const MainDashBoard = () => {
                 </Sidebar>
                 <main style={{ width: "100%" }}>
                     <Header />
+<div style={{height:"120vh"}}>
+
+    sddjfjhdsf
+</div>
                 </main>
             </div>
         </ProSidebarProvider>
