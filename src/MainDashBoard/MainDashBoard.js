01e8b9ef5b8995efbd07b5fb199003fb1fd45d52
Mynul-Dev <mynul.ir@tudotechnologies.com>
2022-11-28 00:16:06 +0600

    submenu created on sidebar

--- src/MainDashBoard/MainDashBoard.js
+++ src/MainDashBoard/MainDashBoard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem,SubMenu } from 'react-pro-sidebar';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
 import Header from '../Component/Header/Headers';
 import brandlogo from '../assets/images/brandLogo.png'
-import { FaDashcube,FaCartPlus,FaListAlt,FaChartPie } from "react-icons/fa";
+import { FaDashcube, FaCartPlus, FaListAlt, FaChartPie } from "react-icons/fa";
 
 
 
@@ -17,22 +17,51 @@ const MainDashBoard = () => {
     return (
         <ProSidebarProvider>
             <div style={{ width: "100%", display: 'flex', height: '100%', minHeight: '100vh' }}>
-                <Sidebar style={{ width: "300px" ,boxShadow: "0px 16px 15px 0px rgb(0 0 0 / 7%)" }}>
+                <Sidebar style={{ width: "300px", boxShadow: "0px 16px 15px 0px rgb(0 0 0 / 7%)" }}>
                     <div className='d-flex align-items-center justify-content-center'>
                         <img width="200px" src={brandlogo} className="py-4 img-fluid mx-auto" alt="" />
                     </div>
                     <Menu className='py-2'>
-                        <MenuItem routerLink={<Link to="/DashBoard" />}> Dashboards</MenuItem>
-                        <MenuItem routerLink={<Link to="/myprofile" />}> Products</MenuItem>
-                        <MenuItem routerLink={<Link to="/myorders" />}>Categories</MenuItem>
-                        <MenuItem routerLink={<Link to="/ledger" />}> Sale</MenuItem>
-                        <MenuItem routerLink={<Link to="/required-medicine" />}>Purchases</MenuItem>
-                        <MenuItem routerLink={<Link to="/contact-us" />}> Returns</MenuItem>
-                        <MenuItem routerLink={<Link to="/contact-us" />}> People</MenuItem>
-                        <MenuItem routerLink={<Link to="/contact-us" />}> Reports</MenuItem>
-                        <MenuItem routerLink={<Link to="/contact-us" />}> Other Page</MenuItem>
-                        <MenuItem > Language</MenuItem>
-                        <MenuItem className='text-danger'> Sign Out</MenuItem>
+
+                        <SubMenu icon={<FaDashcube />} label="Dashboards">
+                            <MenuItem> Pie charts</MenuItem>
+                            <MenuItem> Line charts</MenuItem>
+                            <MenuItem> Bar charts</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube />} label="Products">
+                            <MenuItem> Google maps</MenuItem>
+                            <MenuItem> Open street maps</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube/>} label="Categories">
+                            <MenuItem> Dark</MenuItem>
+                            <MenuItem> Light</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube />} label="Sale">
+                            <MenuItem> Pie charts</MenuItem>
+                            <MenuItem> Line charts</MenuItem>
+                            <MenuItem> Bar charts</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube />} label="Purchases">
+                            <MenuItem> Google maps</MenuItem>
+                            <MenuItem> Open street maps</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube/>} label="Returns">
+                            <MenuItem> Dark</MenuItem>
+                            <MenuItem> Light</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube />} label="People">
+                            <MenuItem> Pie charts</MenuItem>
+                            <MenuItem> Line charts</MenuItem>
+                            <MenuItem> Bar charts</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube />} label="Reports">
+                            <MenuItem> Google maps</MenuItem>
+                            <MenuItem> Open street maps</MenuItem>
+                        </SubMenu>
+                        <SubMenu icon={<FaDashcube/>} label="Other Page">
+                            <MenuItem> Dark</MenuItem>
+                            <MenuItem> Light</MenuItem>
+                        </SubMenu>
                     </Menu>
                 </Sidebar>
                 <main style={{ width: "100%" }}>
