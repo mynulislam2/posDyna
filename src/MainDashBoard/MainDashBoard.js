888ffa9bc4ede6286a6ff765a1ffde690d739701
Mynul-Dev <mynul.ir@tudotechnologies.com>
2022-12-04 15:52:10 +0600

    sidebar fix

--- src/MainDashBoard/MainDashBoard.js
+++ src/MainDashBoard/MainDashBoard.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import Header from '../Component/Header/Headers';
 import brandlogo from '../assets/images/brandLogo.png'
 import { FaDashcube, FaCartPlus, FaListAlt, FaChartPie } from "react-icons/fa";
-
+import './MainDash.css'
 
 
 
@@ -17,9 +17,9 @@ const MainDashBoard = () => {
     return (
         <ProSidebarProvider >
 
-            <div  style={{ display: 'flex', height: '100%', minHeight: '100vh' }}>
 
-                <Sidebar    style={{ boxShadow: "0px 16px 15px 0px rgb(0 0 0 / 7%)" }}>
+            <div className='flex '>
+                <Sidebar style={{ boxShadow: "0px 16px 15px 0px rgb(0 0 0 / 7%)",overflow:'hidden', top: 0, position: "sticky", height: "100vh", width: "25%" }}>
                     <div className='d-flex align-items-center justify-content-center '>
                         <img width="200px" src={brandlogo} className="py-4 img-fluid mx-auto" alt="" />
                     </div>
@@ -67,12 +67,12 @@ const MainDashBoard = () => {
                         </SubMenu>
                     </Menu>
                 </Sidebar>
-                <main  style={{ width: "100%" }}>
+                <main style={{ width: "75%", overflowY: "none" }}>
                     <Header />
-<div  style={{height:"120vh"}}>
+                    <div style={{ height: "120vh" }}>
 
-    sddjfjhdsf
-</div>
+                        sddjfjhdsf
+                    </div>
                 </main>
             </div>
         </ProSidebarProvider>
